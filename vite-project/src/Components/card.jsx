6d82a6431aebbe0767daa0cf7f7250fe76afdf6b
Pratik-Gaikwad-Project <pratik.g.student@gmail.com>
2026-04-27 20:26:54 +0530

    functionality added

---
+++ vite-project/src/Components/card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function card(props) {
+  return (
+    <>
+      <p>
+        <h1>{props.name}</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem culpa
+        asperiores doloremque unde hic architecto cum vel perspiciatis quasi
+        dolor laboriosam, natus dolorum fugiat consequatur voluptate ab ut.
+        Eos, eaque.
+      </p>
+    </>
+  );
+}
+
+export default card;
